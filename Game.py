2b31a9629480652d7f57bb4996a0969496dcdf56
José Neto <jgsnto.sd@gmail.com>
2022-03-01 22:17:16 -0300

    Finish the V1

--- Game.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 28 22:07:48 2022
-
-@author: Jose Gomes
-"""
-
--- teste.py
+++ teste.py
@@ -8,6 +8,6 @@ Created on Tue Mar  1 09:19:44 2022
 from models.calcular import Calcular
 
 #Instanciar objeto
-calc: Calcular = Calcular(dificuldade = 1)
+calc: Calcular = Calcular(1)
 
-print(calc.dificuldade)
+print(calc.valor1)
